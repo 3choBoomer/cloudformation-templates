9510480b0393f77aa66592ac73213668d6eeff75
3choboomer <nathancooke@gmail.com>
2020-02-24 22:33:52 -0600

    Reorg, format  and add Redis template

--- createStack.sh
+++ createStack.sh
@@ -3,6 +3,6 @@ aws cloudformation create-stack --stack-name ci-master-elbDemo \
 --parameters \
 ParameterKey=keyName,ParameterValue=ndcDefaultKey \
 ParameterKey=instanceSecurityGroups,ParameterValue=sg-4c40642b \
-ParameterKey=vpcZoneIdentifiers,ParameterValue=subnet-6774fe2b\\,subnet-a2e115c9\\,subnet-a4e3c1de \
+ParameterKey=subnetIds,ParameterValue=subnet-6774fe2b\\,subnet-a2e115c9\\,subnet-a4e3c1de \
 ParameterKey=templatePath,ParameterValue=https://ndc-cf-templates.s3.us-east-2.amazonaws.com \
 ParameterKey=vpcId,ParameterValue=vpc-49af6322
--- uploadToAws.sh
+++ uploadToAws.sh
@@ -1,1 +1,1 @@
-aws s3 cp . s3://ndc-cf-templates/ --exclude "*" --include "*.yml" --recursive
+aws s3 cp ./templates/. s3://ndc-cf-templates/ --exclude "*" --include "*.yml" --recursive
